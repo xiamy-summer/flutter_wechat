74657f70053d5b73bc4a83a1c21918a6047fad26
598558982@qq.com <cm13512779673>
2019-06-14 15:42:23 +0800

    新增个人信息页面

---
+++ lib/pages/personal_information.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:wechat/widget/appbar_widget.dart';
+
+/// @author SumMer
+/// @date 2019-06-14 0149:26
+/// @Description: 个人信息页面
+
+class PersionalInfoWidget extends StatefulWidget {
+  //头像
+  String headPortrait;
+
+  PersionalInfoWidget(this.headPortrait);
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return new _PersionalInfoWidgetState();
+  }
+}
+
+class _PersionalInfoWidgetState extends State<PersionalInfoWidget> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBarWidget(
+        title: "个人信息",
+      ),
+      body: new ListView(
+        children: <Widget>[
+          _cardbeltwidget(
+            "头像",
+            new ClipRRect(
+              borderRadius: BorderRadius.circular(16.0),
+              child: new Image.asset(
+                widget.headPortrait,
+                width: 70,
+                height: 70,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          _cardbeltwidget(
+            "姓名",
+            new Text("SumMer"),
+          ),
+          _cardbeltwidget(
+            "微信号",
+            new Text("123456"),
+          ),
+          _cardbeltwidget(
+            "我的二维码",
+            new Image.asset(
+              "assets/qrcode.png",
+              width: 25,
+              height: 25,
+            ),
+          ),
+          _card("更多"),
+          new Padding(padding: EdgeInsets.only(top: 10.0)),
+          _card("收货地址"),
+          _card("我的发票抬头"),
+        ],
+      ),
+    );
+  }
+
+  Card _card(String context) {
+    return new Card(
+      margin: EdgeInsets.all(0.0),
+      child: new FlatButton(
+        onPressed: () {
+          print("点击");
+        },
+        child: new Container(
+          margin: EdgeInsets.only(top: 10.0, bottom: 10.0),
+          child: new Row(
+            children: <Widget>[
+              new Text(context),
+              new Expanded(
+                child: new Row(
+                  //位置，显示在最右侧
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    new Icon(Icons.arrow_forward_ios),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Card _cardbeltwidget(String context, Widget widget) {
+    return new Card(
+      margin: EdgeInsets.all(0.0),
+      child: new FlatButton(
+        onPressed: () {
+          print("点击");
+        },
+        child: new Container(
+          margin: EdgeInsets.only(top: 10.0, bottom: 10.0),
+          child: new Row(
+            children: <Widget>[
+              new Text(context),
+              new Expanded(
+                child: new Row(
+                  //位置，显示在最右侧
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    widget,
+                    new Padding(padding: EdgeInsets.only(left: 5.0)),
+                    new Icon(Icons.arrow_forward_ios),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
